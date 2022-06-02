e94bcbcb36e93bcbcbeba516978674f5702c11be
SeboMyr <80338167+Seborider@users.noreply.github.com>
2022-06-02 17:38:44 +0200

    🚚 Add paths for signin and signup

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'signup', component: SignupComponent
+  },
+  {
+    path: '', component: SigninComponent
+  }
+];
+
+
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
