f690a54beac2a0d6b5530bb38d6ac479733be6ab
SeboMyr <80338167+Seborider@users.noreply.github.com>
2022-06-02 17:52:31 +0200

    🎨 Add form with inputs

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { SigninComponent } from './signin/signin.component';
@@ -13,7 +14,8 @@ import { SignupComponent } from './signup/signup.component';
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class AuthModule { }
--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
-
+  authForm = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl(''),
+    passwordConfirmation: new FormControl('')
+  })
   constructor() { }
 
   ngOnInit(): void {
