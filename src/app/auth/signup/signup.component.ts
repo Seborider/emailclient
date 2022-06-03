8249bc0b35ae53eb2997552dd09d4e5db3425a08
SeboMyr <80338167+Seborider@users.noreply.github.com>
2022-06-03 09:43:10 +0200

    🎨 Import and use DI for MatchPassword class

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { MatchPassword } from '../validators/match-password';
 
 @Component({
   selector: 'app-signup',
@@ -24,8 +25,11 @@ export class SignupComponent implements OnInit {
       Validators.minLength(4),
       Validators.maxLength(20),
     ])
+  }, 
+  { validators:[this.matchPassword.validate]
   })
-  constructor() { }
+
+  constructor(private matchPassword: MatchPassword) { }
 
   ngOnInit(): void {
   }
