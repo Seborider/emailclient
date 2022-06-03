c24a1aca981f0853e45dd482fd10ab3c39f20816
SeboMyr <80338167+Seborider@users.noreply.github.com>
2022-06-03 10:52:45 +0200

    🎨 Add uniqueUsername class and implement AsyncValidation

--- src/app/auth/validators/unique-username.ts
+++ src/app/auth/validators/unique-username.ts
@@ -1,2 +1,19 @@
-export class UniqueUsername {
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { FormControl, AsyncValidator, AbstractControl } from "@angular/forms";
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class UniqueUsername implements AsyncValidator {
+    constructor(private http: HttpClient) {}
+
+    validate = (control: AbstractControl) => {
+        const { value } = control
+        console.log(this.http,);
+        return control.value
+            
+        }
 }
+
