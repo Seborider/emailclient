5c4f35b69489afab418eca4fd01c55a502fc0151
SeboMyr <80338167+Seborider@users.noreply.github.com>
2022-06-07 17:43:29 +0200

    🎨 Import EmailService to DI and subscribe to getEmails

--- src/app/inbox/email-index/email-index.component.ts
+++ src/app/inbox/email-index/email-index.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EmailService } from '../email.service';
 
 @Component({
   selector: 'app-email-index',
@@ -6,10 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./email-index.component.css']
 })
 export class EmailIndexComponent implements OnInit {
+  constructor(private emailService: EmailService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.emailService.getEmails().subscribe(()=>{})
   }
 
 }
