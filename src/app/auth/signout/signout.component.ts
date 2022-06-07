53180360d645824a97cefb39ec94fd7224f91439
SeboMyr <80338167+Seborider@users.noreply.github.com>
2022-06-07 11:55:58 +0200

    🎨 Import router and DI into signout

--- src/app/auth/signout/signout.component.ts
+++ src/app/auth/signout/signout.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signout',
@@ -8,11 +9,13 @@ import { AuthService } from '../auth.service';
 })
 export class SignoutComponent implements OnInit {
 
-  constructor(private authService: AuthService) { }
+  constructor(
+    private authService: AuthService,
+    private router: Router) { }
 
   ngOnInit(): void {
     this.authService.signout().subscribe(() => {
-      //Navigate 
+      this.router.navigateByUrl('/')
     })
   }
 
