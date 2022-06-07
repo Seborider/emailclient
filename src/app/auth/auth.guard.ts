7442abd4689e4d5711441a05fd81bc97d1eef3ae
SeboMyr <80338167+Seborider@users.noreply.github.com>
2022-06-07 15:41:03 +0200

    ⚡️ Import authService to DI on authGuard and add operators skipWhile, map, take to fake signedin status

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,17 +1,23 @@
 import { Injectable } from '@angular/core';
 import { CanLoad, Route, UrlSegment, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+import { take, skipWhile, map } from 'rxjs/operators'
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanLoad {
+  constructor(private authService: AuthService) {}
+
   canLoad(
     route: Route,
     segments: UrlSegment[]): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return new Observable(subscriber => {
-      subscriber.next(true)
-      subscriber.complete()
-    })
+    return this.authService.signedin$.pipe(
+      skipWhile((value) => value === null),
+      map((value) => value!),
+      take(1)
+    )
   }
 }
