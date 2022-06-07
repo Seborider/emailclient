ee5e41ca2afef2b69e423f7df48814ea5ab13128
SeboMyr <80338167+Seborider@users.noreply.github.com>
2022-06-07 11:48:17 +0200

    🚚 Add signout route

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
+import { SignoutComponent } from './signout/signout.component';
 
 const routes: Routes = [
   {
@@ -9,6 +10,9 @@ const routes: Routes = [
   },
   {
     path: '', component: SigninComponent
+  },
+  {
+    path: 'signout', component: SignoutComponent
   }
 ];
 
