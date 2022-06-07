f84cc7fc355d943154cf12cf055c2e0c89e03cd3
SeboMyr <80338167+Seborider@users.noreply.github.com>
2022-06-07 11:49:20 +0200

    🎨 Import AuthService and DI into component

--- src/app/auth/signout/signout.component.ts
+++ src/app/auth/signout/signout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-signout',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignoutComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
+    this.authService.signout().subscribe(() => {
+      //Navigate 
+    })
   }
 
 }
