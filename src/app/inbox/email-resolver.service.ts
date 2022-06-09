bc9e6941fa4542bde454919fc07a00a851091b96
SeboMyr <80338167+Seborider@users.noreply.github.com>
2022-06-09 11:00:06 +0200

    🤡 Import Email interface and Resolve and mock data in resolve method

--- src/app/inbox/email-resolver.service.ts
+++ src/app/inbox/email-resolver.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Resolve } from '@angular/router'
+import { Email } from './email';
 
 @Injectable({
   providedIn: 'root'
 })
-export class EmailResolverService {
+export class EmailResolverService implements Resolve<Email>{
 
   constructor() { }
+
+  resolve() {
+      return {
+        id: 'asda',
+        subject: 'adsa',
+        to: 'asd',
+        from: 'ad',
+        text: 'aasdd',
+        html: 'aad'
+      }
+  }
 }
