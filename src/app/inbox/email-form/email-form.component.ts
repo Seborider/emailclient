0f2540fb5e1f9eff8524f22bab2514c33f9f8293
SeboMyr <80338167+Seborider@users.noreply.github.com>
2022-06-09 15:11:17 +0200

    🎨 Initialize FormGroup

--- src/app/inbox/email-form/email-form.component.ts
+++ src/app/inbox/email-form/email-form.component.ts
@@ -8,11 +8,20 @@ import { FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./email-form.component.css']
 })
 export class EmailFormComponent implements OnInit {
+  emailForm!: FormGroup
   @Input() email!: Email
 
   constructor() { }
 
   ngOnInit(): void {
+    const { subject, from, to, text } = this.email
+
+    this.emailForm = new FormGroup({
+      to: new FormControl(to),
+      from: new FormControl(from),
+      subject: new FormControl(subject),
+      text: new FormControl(text)
+    })
   }
 
 }
