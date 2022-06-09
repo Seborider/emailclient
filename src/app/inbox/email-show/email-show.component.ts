03495818e9fd2937b27eec8ea9638889256e7550
SeboMyr <80338167+Seborider@users.noreply.github.com>
2022-06-09 11:33:38 +0200

    ♻️ Refactor to show emails

--- src/app/inbox/email-show/email-show.component.ts
+++ src/app/inbox/email-show/email-show.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { EmailService } from '../email.service';
-import { switchMap } from 'rxjs';
+
 import { Email } from '../email'
 
 @Component({
@@ -14,23 +13,12 @@ export class EmailShowComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute, 
-    private emailService: EmailService
     ) {
-      this.route.data.subscribe((data) => {
-        console.log(data);
-        
+      this.email = route.snapshot.data['email'];
+      this.route.data.subscribe(({ email }) => {
+        this.email = email
       })
-      
-     }
+    }
 
-  ngOnInit(): void {
-    
-    // this.route.params.pipe(
-    //   switchMap(({ id }) => {
-    //     return this.emailService.getEmail(id)
-    //   })
-    // ).subscribe((email) => {this.email = email;
-    // })
-  }
-
+  ngOnInit(): void {}
 }
