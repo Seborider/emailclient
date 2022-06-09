c007e49d853c83c4ce40fb55098ec3ef1a8ce1be
SeboMyr <80338167+Seborider@users.noreply.github.com>
2022-06-09 09:48:45 +0200

    🎨 Import Acticated Route, EmailInterface, EmailService, switchMap to implement getEmail function

--- src/app/inbox/email-show/email-show.component.ts
+++ src/app/inbox/email-show/email-show.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { EmailService } from '../email.service';
+import { switchMap } from 'rxjs';
+import { Email } from '../email'
 
 @Component({
   selector: 'app-email-show',
@@ -6,10 +10,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./email-show.component.css']
 })
 export class EmailShowComponent implements OnInit {
+  email!: Email;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private emailService: EmailService) { }
 
   ngOnInit(): void {
+    
+    this.route.params.pipe(
+      switchMap(({ id }) => {
+        return this.emailService.getEmail(id)
+      })
+    ).subscribe((email) => {this.email = email;
+    })
   }
 
 }
