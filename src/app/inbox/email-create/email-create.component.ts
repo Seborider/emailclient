899397188428b9aced6b37d4bc5f566b4dfae309
SeboMyr <80338167+Seborider@users.noreply.github.com>
2022-06-09 14:39:46 +0200

    🎨 Add showModal property

--- src/app/inbox/email-create/email-create.component.ts
+++ src/app/inbox/email-create/email-create.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./email-create.component.css']
 })
 export class EmailCreateComponent implements OnInit {
+  showModal = false
 
   constructor() { }
 
