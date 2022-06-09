d416d95859f33fc3a529d0cba87d621cbb37928a
SeboMyr <80338167+Seborider@users.noreply.github.com>
2022-06-09 16:57:30 +0200

    🎨 Add properties

--- src/app/inbox/email-reply/email-reply.component.ts
+++ src/app/inbox/email-reply/email-reply.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Email } from '../email';
 
 @Component({
   selector: 'app-email-reply',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./email-reply.component.css']
 })
 export class EmailReplyComponent implements OnInit {
+  showModal = false
+  email!: Email
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSubmit(email: Email) {}
 }
