2b1bf0dcd46ead1f30fecbb74b8c80a14c57dcc3
SeboMyr <80338167+Seborider@users.noreply.github.com>
2022-06-09 14:19:30 +0200

    🎨 Import sharedModule

--- src/app/inbox/inbox.module.ts
+++ src/app/inbox/inbox.module.ts
@@ -9,6 +9,7 @@ import { EmailIndexComponent } from './email-index/email-index.component';
 import { EmailShowComponent } from './email-show/email-show.component';
 import { PlaceholderComponent } from './placeholder/placeholder.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
@@ -23,7 +24,8 @@ import { NotFoundComponent } from './not-found/not-found.component';
   ],
   imports: [
     CommonModule,
-    InboxRoutingModule
+    InboxRoutingModule,
+    SharedModule
   ]
 })
 export class InboxModule { }
