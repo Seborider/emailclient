be8a7559c35584fb5da5ad8d0733897a2d65d7a4
SeboMyr <80338167+Seborider@users.noreply.github.com>
2022-06-09 16:39:13 +0200

    🎨 Add onSubmit

--- src/app/inbox/email-create/email-create.component.ts
+++ src/app/inbox/email-create/email-create.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Email } from '../email';
 import { AuthService } from 'src/app/auth/auth.service';
+import { EmailService } from '../email.service';
 
 @Component({
   selector: 'app-email-create',
@@ -11,7 +12,9 @@ export class EmailCreateComponent implements OnInit {
   showModal = false
   email: Email
 
-  constructor(private authService: AuthService) {
+  constructor(
+    private authService: AuthService,
+    private emailService: EmailService) {
     this.email = {
       id: '',
       to: '',
@@ -25,4 +28,9 @@ export class EmailCreateComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onSubmit(email: Email) {
+    this.emailService.sendEmail(email).subscribe(() => {
+      this.showModal = false
+    })
+  }
 }
